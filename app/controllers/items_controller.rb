92af75cfe05d1a63d7be432daeb91c61762ec7cd
necori <topodebtk7@yahoo.co.jp>
2024-07-25 00:33:37 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new,:create]
   before_action :move_to_index, except: [:index, :show, :new, :create]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item,only:[:show, :edit, :update]
 
  def index
   @items = Item.all.order('created_at DESC')
@@ -25,7 +25,12 @@ class ItemsController < ApplicationController
 
  private
  
-   def item_params
+  def item_params
     params.require(:item).permit(:image, :product_name, :product_description, :category_id, :condition_id, :shipping_id, :prefecture_id, :duration_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
  end
