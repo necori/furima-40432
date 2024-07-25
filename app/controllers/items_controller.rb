829bf1f623082277b23fabec7e72713d2493f44f
necori <topodebtk7@yahoo.co.jp>
2024-07-25 19:02:57 +0900

    レビュー対応・その1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new,:create]
   before_action :move_to_index, except: [:index, :show, :new, :create]
-  before_action :set_item,only:[:show, :edit, :update]
+  before_action :set_item,only:[:show]#:edit, :update]
 
  def index
   @items = Item.all.order('created_at DESC')
