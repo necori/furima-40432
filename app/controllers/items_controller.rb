31e9856ee673c212ba93a66bdba300396eff961d
necori <topodebtk7@yahoo.co.jp>
2024-07-27 17:50:12 +0900

    レビュー対応・その1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new,:create,:edit,:update]
-  before_action :move_to_index, except: [:index, :show, :new, :create]
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
+  before_action :move_to_index, except:[:index, :show, :new, :create, :destroy]
   before_action :set_item, only:[:show, :edit, :update, :destroy]
     
  def index
